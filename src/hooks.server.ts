59e1a6d711fe1ae706fbdd80d8806dd7565020c9
Nic <162764842+Nic-Polumeyv@users.noreply.github.com>
2026-08-11 19:34:55 -0400

    kit next.20: Handle type moved to @sveltejs/kit/hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import type { Handle } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit/hooks';
 import { assetPreload } from '@polumeyv/lib/kit/asset-preload';
 
 export const handle: Handle = ({ event, resolve }) => resolve(event, assetPreload);
